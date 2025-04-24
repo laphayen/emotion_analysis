8da78aaaf4ddf77583d1d2cc2c723ecaa441ea2d
Baek Gi Chan <laphayen@gmail.com>
2025-04-24 17:41:28 +0900

    Feat: KoBERT 감정 분석 학습용 EmotionDataset 클래스 추가
    HuggingFace Transformers의 `BertTokenizer`를 활용하여 텍스트를 토크나이즈
    PyTorch Dataset 클래스를 상속하여 모델 학습용 텍스트-감정 레이블 쌍 구성
    max_length 기반 padding/truncation 처리 포함

---
+++ dataset.py
@@ -0,0 +1,29 @@
+
+# dataset.py
+
+from torch.utils.data import Dataset
+from transformers import BertTokenizer
+
+class EmotionDataset(Dataset):
+    def __init__(self, texts, labels, tokenizer, max_len):
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        encoded = self.tokenizer(
+            self.texts[idx],
+            truncation=True,
+            padding='max_length',
+            max_length=self.max_len,
+            return_tensors='pt'
+        )
+        return {
+            'input_ids': encoded['input_ids'].squeeze(),
+            'attention_mask': encoded['attention_mask'].squeeze(),
+            'labels': self.labels[idx]
+        }
